78f91582b51aa54fd9105186353de0fa67779d39
Prem6d <98529369+Prem6d@users.noreply.github.com>
2026-08-10 18:14:39 +0530

    Implement isAnagram method to check anagrams

---
+++ Anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) { 
+
+        if(s.length() != t.length())
+        return false;
+
+        HashMap<Character,Integer> a = new HashMap<>();
+        HashMap<Character,Integer> b = new HashMap<>();
+
+        for(int i = 0 ; i < s.length() ; i++){
+            a.put(s.charAt(i), a.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i = 0 ; i < t.length() ; i++){
+            b.put(t.charAt(i), b.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        return a.equals(b);
+
+    }
+    
+}
